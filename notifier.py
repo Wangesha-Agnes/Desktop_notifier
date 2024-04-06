b13ae458a841a31d2d1e102da92bda595dc435e1
Wangesha-Agnes <wangeshaagnes@gmail.com>
2024-04-06 19:04:48 +0300

    added my project

--- notifier.py
+++ notifier.py
@@ -1,27 +1,13 @@
-# from plyer import notification  import time
-
-# title = 'Desktop Notifier'
-# message = 'This is a notification from your python desktop notifier!'
-
-# notification.notify(title=title,
-#                     message=message,
-#                     timeout=10) #Notification will stay for 10 seconds
-
-# time.sleep(15)#Wait for 15 seconds before closing the notification
-
-From plyer import notification 
+from plyer import notification
 import time
-
-def desktop_notifier(title,message):
+def desktop_notifier(title, message):
     notification.notify(
         title=title,
         message=message,
         app_name="Desktop Notifier",
         timeout=10
     )
-    
-if _name_=="_main_":
-    title="Notification Title"
-    message="This is a desktop notification message."
-    desktop_notifier(title,message)
-       
+if __name__ == "__main__":
+    title = "Notification Title"
+    message = "This is a desktop notification message."
+    desktop_notifier(title, message)
